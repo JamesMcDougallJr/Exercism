747a6876577f460589239e99906fe5315bf85503
JamesMcDougallJr <jmcdouga@ucsd.edu>
2020-01-08 16:34:42 -0800

    Finished armstrong_numbers, moving on to darts

--- c/armstrong-numbers/src/armstrong_numbers.c
+++ c/armstrong-numbers/src/armstrong_numbers.c
@@ -6,13 +6,25 @@
 int is_armstrong_number(int candidate) {
   printf("Candidate: %d\n", candidate);
   printf("Candidate / 10: %d\n", candidate/10);
-  int candidateTo0 = candidate;
+  int candidateTo0 = candidate/10;
   int numDigits = 1;
   while(candidateTo0 > 0) {
     numDigits++;
     candidateTo0 /= 10;
   }
   printf("numDigits: %d\n", numDigits);
-  
-  return 0;
+
+  int sum = 0;
+  candidateTo0 = candidate;
+  while (candidateTo0 > 0) {
+    int lastDigit = candidateTo0 % 10;
+    candidateTo0 /= 10;
+    int toAddToSum = 1;
+    for(int i = 0; i < numDigits; i++) {
+      toAddToSum *= lastDigit;
+    } 
+    sum += toAddToSum;
+  }
+  printf("Sum: %d\n", sum);
+  return sum == candidate;
 }
