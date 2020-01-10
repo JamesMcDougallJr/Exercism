3a688038a2667dd879813aecb5c3a8e96be48d7d
JamesMcDougallJr <jmcdouga@ucsd.edu>
2020-01-09 17:30:13 -0800

    Updated darts

--- c/darts/src/darts.c
+++ c/darts/src/darts.c
@@ -1,1 +1,9 @@
 #include "darts.h"
+
+unsigned char score(coordinate_t pos) {
+  float squared_distance = pos.x * pos.x + pos.y * pos.y;
+  if (squared_distance <= CENTER*CENTER) return CENTER_POINTS;
+  else if (squared_distance <= MIDDLE*MIDDLE) return MIDDLE_POINTS;
+  else if (squared_distance <= OUTER*OUTER) return OUTER_POINTS;
+  return OUT_OF_BOUNDS_POINTS;
+}
--- c/darts/src/darts.h
+++ c/darts/src/darts.h
@@ -1,4 +1,20 @@
 #ifndef DARTS_H
 #define DARTS_H
 
+#define CENTER_POINTS 10
+#define MIDDLE_POINTS 5
+#define OUTER_POINTS 1
+#define OUT_OF_BOUNDS_POINTS 0
+
+#define OUTER 10
+#define MIDDLE 5
+#define CENTER 1
+
+typedef struct {
+  float x;
+  float y;
+} coordinate_t;
+
+typedef unsigned char uint8_t;
+uint8_t score(coordinate_t landingPosition);
 #endif
