047b260bff651a3d675cc4211ab2e04c99adabac
JamesMcDougallJr <jmcdouga@ucsd.edu>
2020-01-10 17:06:58 -0800

    Finished first resistor colors

--- c/resistor-color/src/resistor_color.c
+++ c/resistor-color/src/resistor_color.c
@@ -1,1 +1,5 @@
 #include "resistor_color.h"
+
+const resistor_band_t * colors() {return colorList;}
+
+int color_code(resistor_band_t code) {return code;}
--- c/resistor-color/src/resistor_color.h
+++ c/resistor-color/src/resistor_color.h
@@ -2,20 +2,21 @@
 #define RESISTOR_COLOR_H
 typedef int resistor_band_t; 
 
-#define BLACK 0
-#define BROWN 1
-#define RED 2
-#define ORANGE 3
-#define YELLOW 4
-#define GREEN 5
-#define BLUE 6
-#define VIOLET 7
-#define GREY 8
-#define WHITE 9
-const resistor_band_t colorsList[10] = {BLACK, BROWN, RED, ORANGE, YELLOW, GREEN, BLUE, VIOLET, GREY };
-const resistor_band_t * colors() {
-  return colorsList;
-}
+enum Color { BLACK = 0,
+  BROWN = 1,
+  RED = 2,
+  ORANGE = 3,
+  YELLOW = 4,
+  GREEN = 5,
+  BLUE = 6,
+  VIOLET = 7,
+  GREY = 8,
+  WHITE = 9,
+};
+
+static int colorList[] = {BLACK, BROWN, RED, ORANGE, YELLOW, GREEN, BLUE, VIOLET, GREY, WHITE};
+
+const resistor_band_t * colors();
 int color_code(resistor_band_t code);
 
 #endif
