b89b7fb22296a7a4ed8b232b40ab4019ef01de8e
JamesMcDougallJr <jmcdouga@ucsd.edu>
2020-01-11 10:48:29 -0800

    Finished resistor-color-duo

--- c/resistor-color-duo/src/resistor_color_duo.c
+++ c/resistor-color-duo/src/resistor_color_duo.c
@@ -1,1 +1,13 @@
 #include "resistor_color_duo.h"
+#include<stdio.h>
+
+/*
+ * returns the code created by the first two elements in the input array.
+ * If the 1st or 2nd array value is garbage, it will return a garbage value
+ */
+uint16_t color_code (resistor_band_t bands[]) {
+  int colorCodeSum = 0;
+  colorCodeSum += (10*bands[0]);
+  colorCodeSum += bands[1];
+  return colorCodeSum;
+}
--- c/resistor-color-duo/src/resistor_color_duo.h
+++ c/resistor-color-duo/src/resistor_color_duo.h
@@ -1,4 +1,20 @@
 #ifndef RESISTOR_COLOR_DUO_H
 #define RESISTOR_COLOR_DUO_H
 
+typedef enum resistor_band_t {
+  BLACK = 0,
+  BROWN = 1,
+  RED = 2,
+  ORANGE = 3,
+  YELLOW = 4,
+  GREEN = 5,
+  BLUE = 6,
+  VIOLET = 7,
+  GREY = 8,
+  WHITE = 9,
+} resistor_band_t;
+
+typedef unsigned short uint16_t;
+uint16_t color_code(resistor_band_t[]);
+
 #endif
