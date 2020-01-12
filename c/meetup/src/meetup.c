f887490a099b924d861b097e8874290130086546
JamesMcDougallJr <jmcdouga@ucsd.edu>
2020-01-12 11:47:15 -0800

    Update meetup

--- c/meetup/src/meetup.c
+++ c/meetup/src/meetup.c
@@ -1,1 +1,53 @@
 #include "meetup.h"
+
+/**
+ * meetup_day_of_month returns the date of the month from the given info
+ * Param year is the year
+ * Param month is the month in that year
+ * Param week is the week within the month
+ * Param day_of_week is the name of the day of the week
+ */
+int meetup_day_of_month(unsigned int year, unsigned int month, const char *week,
+                        const char *day_of_week)
+{
+  int first_day = get_first_day(year); // first "day of week" of the year
+  Month current_month = Jan;
+  int number_of_days_passed = 0;
+  while (current_month != (Month) month) {
+    number_of_days_passed += days_in_month(current_month);
+    current_month += 1;
+  }
+
+  /**
+   * Get the first "day of week" of the given month so I can count up from
+   * there
+   */
+  char *first_day_of_month = get_first_day_of_month(number_of_days_passed, first_day);
+
+  return -1;
+}
+
+/**
+ * Get the first day of the given month
+ * Param num_days is the number of days since the beginning of the year
+ * Param first_day is the "day of the week" of the first day of the year
+ */
+char *get_first_day_of_month(int num_days, char *first_day) {
+  int num_weeks = num_days / DAYS_IN_WEEK;
+  int remaining_days = num_days % DAYS_IN_WEEK;
+  // TODO Compute the "day of week" of the first day in the month
+}
+
+/**
+ * Get the first day's day_of_week for the given year
+ * Param year is the integer year, e.g. 2016, 2017, 2018
+ */
+char *get_first_day_of_year(int year) {
+  for(int i = 0; i < NUMYEARS; i++) {
+    if(firstDays[0].year == year) {
+      return firstDays[0].day_of_week;
+    }
+  }
+  return 0;
+}
+
--- c/meetup/src/meetup.h
+++ c/meetup/src/meetup.h
@@ -1,7 +1,50 @@
 #ifndef MEETUP_H
 #define MEETUP_H
 
+#define NUMYEARS 1
+#define DAYS_IN_WEEK 7
+
+typedef enum Month
+{
+  Jan = 0,
+  Feb = 1,
+  Mar = 2,
+  Apr = 3,
+  May = 4,
+  Jun = 5,
+  Jul = 6,
+  Aug = 7,
+  Sep = 8,
+  Oct = 9,
+  Nov = 10,
+  Dec = 11,
+} Month;
+
+//  the number of days in each month, in a non leap year
+Month days_in_month[12] = {
+  31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
+};
+
+typedef struct first_day {
+  int year;
+  char *day_of_week;
+} first_day;
+
+first_day firstDays[] = {
+  {.year = 2013, .day_of_week = "Tuesday"}
+};
+
+/**
+ * Get the day of month from the given information
+ */
 int meetup_day_of_month(unsigned int year, unsigned int month, const char *week,
                         const char *day_of_week);
+
+char *get_first_day_of_year(int year);
+
+/**
+ * 
+ */
+char *get_first_day_of_month(int num_days, char *first_day);
 
 #endif
