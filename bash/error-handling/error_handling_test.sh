36e6d870324e4d9b0e6ef88d71c78de91af3ac74
JamesMcDougallJr <jmcdouga@ucsd.edu>
2020-01-18 23:31:38 -0800

    finished a quick bash and added a medium java

--- bash/error-handling/error_handling_test.sh
+++ bash/error-handling/error_handling_test.sh
@@ -3,23 +3,18 @@
 # local version: <none>
 
 @test "correct arguments" {
-  #[[ $BATS_RUN_SKIPPED == true  ]] || skip
   run bash error_handling.sh Alice
-
   [[ $status -eq 0 ]]
   [[ $output = "Hello, Alice" ]]
 }
 
 @test "one long argument" {
-  [[ $BATS_RUN_SKIPPED == true  ]] || skip
   run bash error_handling.sh "Alice and Bob"
-
   [[ $status -eq 0 ]]
   [[ $output = "Hello, Alice and Bob" ]]
 }
 
 @test "incorrect arguments" {
-  [[ $BATS_RUN_SKIPPED == true  ]] || skip
   run bash error_handling.sh Alice Bob
 
   [[ $status -eq 1 ]]
@@ -27,7 +22,6 @@
 }
 
 @test "print usage banner with no value given" {
-  [[ $BATS_RUN_SKIPPED == true  ]] || skip
   run bash error_handling.sh
 
   [[ $status -eq 1 ]]
@@ -35,7 +29,6 @@
 }
 
 @test "empty argument" {
-  [[ $BATS_RUN_SKIPPED == true  ]] || skip
   run bash error_handling.sh ""
 
   [[ $status -eq 0 ]]
