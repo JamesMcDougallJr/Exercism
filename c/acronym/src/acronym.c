704eed1bc90a1fef7f7ec33822facd13de34907f
James I Mcdougall <jmcdouga@ieng6-201.ucsd.edu>
2020-01-23 22:07:16 -0800

    Finished acronym

--- c/acronym/src/acronym.c
+++ c/acronym/src/acronym.c
@@ -1,8 +1,12 @@
 #include "acronym.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
 char *abbreviate(const char *phrase) {
+  if(!phrase) return 0;
+  if(phrase[0] =='\0') return NULL;
   char *abbreviation;
   int i = 0;
   int numChars = 1;
@@ -15,10 +19,10 @@ char *abbreviate(const char *phrase) {
   abbreviation = (char*) malloc(sizeof(char) * numChars);
   i = 1;
   int abbrevIndex = 1;
-  abbreviation[0] = phrase[0];
+  abbreviation[0] = toupper(phrase[0]);
   while(phrase[i] != '\0') {
-    if(phrase[i] == ' ') {
-      abbreviation[abbrevIndex++] = phrase[i+1];
+    if(phrase[i] == ' ' || phrase[i] == '-' ) {
+      abbreviation[abbrevIndex++] = toupper(phrase[i+1]);
     }
     i++;
   }
